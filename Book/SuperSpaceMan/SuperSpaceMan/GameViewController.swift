1f254dac1a53f4d38653662052fcc3b92de6ba83
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2019-12-18 10:21:41 -0500

    First chapter done

--- Book/SuperSpaceMan/SuperSpaceMan/GameViewController.swift
+++ Book/SuperSpaceMan/SuperSpaceMan/GameViewController.swift
@@ -12,39 +12,15 @@ import GameplayKit
 
 class GameViewController: UIViewController {
 
+    var scene: GameScene!
+    override var prefersStatusBarHidden:Bool { return true }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let skView = view as! SKView
+        skView.showsFPS = true
+        scene = GameScene(size:skView.bounds.size)
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
     }
 
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
 }
