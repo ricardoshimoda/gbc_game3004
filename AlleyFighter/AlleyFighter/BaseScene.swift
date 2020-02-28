83309a9b87647795f940b25733b1dcb848a2f32e
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-02-28 10:09:22 -0500

    Alley Fighter

---
+++ AlleyFighter/AlleyFighter/BaseScene.swift
@@ -0,0 +1,9 @@
+//
+//  BaseScene.swift
+//  AlleyFighter
+//
+//  Created by Ricardo Shimoda Nakasako on 2020-02-28.
+//  Copyright © 2020 Ricardo Shimoda Nakasako. All rights reserved.
+//
+
+import Foundation
