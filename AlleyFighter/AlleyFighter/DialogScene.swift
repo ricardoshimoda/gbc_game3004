cdf87b112cecdca1d65fbebf25da06e2cf588efa
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2020-02-27 22:58:44 -0500

    Starting alley fighter

---
+++ AlleyFighter/AlleyFighter/DialogScene.swift
@@ -0,0 +1,9 @@
+//
+//  DialogScene.swift
+//  AlleyFighter
+//
+//  Created by Ricardo Shimoda Nakasako on 2020-02-27.
+//  Copyright © 2020 Ricardo Shimoda Nakasako. All rights reserved.
+//
+
+import Foundation
